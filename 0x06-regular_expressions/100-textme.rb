cde72dd0471861df564a02adbad582e68377897f
Ojay16 <juliusojemare16@gmail.com>
2024-03-15 19:05:36 -0700

    update

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# An expression that matches a given pattern
+# This expression natches a given pattern
 puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
